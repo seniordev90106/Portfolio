aa86fdd01c7cd7f5a0a99e67d976366847b82283
seniordev90106 <seniordev90106@gmail.com>
2023-01-31 19:36:42 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const {Recache} = require('recache-client');
+
+let projectID = 268;
+await Recache.init(process.env.RECACHE_SECRET, projectID);
+
+await Recache.logEvent_AutodetectIp("Hello, world!");
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
